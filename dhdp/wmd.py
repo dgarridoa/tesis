2ca6767fd8f75f5e74222e76a6827028617ea8f7
dgarrido <diego.garrido.6568@gmail.com>
2020-08-04 17:28:08 -0400

    WMD with topic relevance

--- dhdp/wmd.py
+++ dhdp/wmd.py
@@ -38,8 +38,27 @@ def get_quantile(array, q=0.05):
     
     return quantile
 
+def relevance(topic, term_probability, alpha):
+    """
+    Input
+        topic: numpy.array, 
+                topic distribution.
+        alpha: float, optional,
+                weight parameter in [0,1], where alpha determines the weight given to the probability of term w under topic k relative to its lift.
+        term_probabilities: numpy.array,
+                probabilities of each word in the vocabulary
+    Output
+        r: float,
+            relevance = alpha log p(w|k)+(1-alpha)log p(w|k)/p(w).
+            then map relevance to a vector with r[i]>=0 and sum_i r[i]=1.
+    """
+    r = alpha*np.nan_to_num(np.log(topic))+(1-alpha)*np.nan_to_num(np.log(topic/term_probability))
+    r = 1/np.abs(r)
+    r = r/r.sum()
+    r[r<np.finfo(np.float).eps] = 0
+    return r
 
-def wmd(embeddings, token2id1, token2id2, topic1, topic2, q=None):
+def wmd(embeddings, token2id1, token2id2, topic1, topic2, termp1, termp2, q=None, alpha=1):
     """
     Input
         embeddings: gensim.models.keyedvectors.FastTextKeyedVectors, 
@@ -48,16 +67,23 @@ def wmd(embeddings, token2id1, token2id2, topic1, topic2, q=None):
             mapping between tokens and ids from topic 1.
         token2id2: dict[str->int], 
             mapping between tokens and ids from topic 2.
-        topic1: numpy.array[float64], 
+        topic1: numpy.array, 
             distribution from topic 1.
-        topic2: numpy.array[float64], 
+        topic2: numpy.array, 
             distribution from topic 2.
-        q: float64, optional, 
+        termp1: numpy.array,
+            probabilities of each word in the vocabulary of topic 1.
+        termp2: numpy.array,
+            probabilities of each word in the vocabulary of topic 2.
+        q: float, optional, 
             quantile rate between [0-1], used to filter words with less probability in a topic.
             represents the top N most likely words in a topic, such that its cumulative distribution 
             adds up to q.
+        alpha: float, optional,
+            weight parameter, where alpha determines the weight given to the probability of term w under topic k relative to its lift.
+            relevance = alpha log p(w|k)+(1-alpha)log p(w|k)/p(w), alpha in [0,1].   
     Output
-        distance: float64
+        distance: float
             word movers distance between two topics.
     """
     
@@ -80,11 +106,18 @@ def wmd(embeddings, token2id1, token2id2, topic1, topic2, q=None):
         vocabulary1 = set(token2id1.keys())
         vocabulary2 = set(token2id2.keys())
         vocabulary = vocabulary1.union(vocabulary2)
-    # topics distributions with padding
-    first_histogram = np.array([topic1[token2id1[word]] if word in vocabulary1 else 0 for word in vocabulary])
-    first_histogram = first_histogram/first_histogram.sum()
-    second_histogram = np.array([topic2[token2id2[word]] if word in vocabulary2 else 0 for word in vocabulary])    
-    second_histogram = second_histogram/second_histogram.sum()
+    
+    # topics and terms distributions with padding
+    termp1 = np.array([termp1[token2id1[word]] if word in vocabulary1 else 0 for word in vocabulary])
+    topic1 = np.array([topic1[token2id1[word]] if word in vocabulary1 else 0 for word in vocabulary])
+
+    topic2 = np.array([topic2[token2id2[word]] if word in vocabulary2 else 0 for word in vocabulary])    
+    termp2 = np.array([termp2[token2id2[word]] if word in vocabulary2 else 0 for word in vocabulary])    
+    
+    # get term relevances
+    first_histogram = relevance(topic1, termp1, alpha)
+    second_histogram = relevance(topic2, termp2, alpha)
+    
     # compute euclidean distance matrix between vocabulary words
     vocabulary_array = np.array([embeddings[word] for word in vocabulary])
     distance_matrix = pairwise_distances(X=vocabulary_array, metric="euclidean", n_jobs=-1)

aaec1a9fcf56d525d8baa863b7c351a6ac7fe965
Diego Garrido <diego.garrido.6568@gmail.com>
2020-09-23 15:19:25 -0300

    Concurent hpd

--- src/dhdp/run_hdp.sh
+++ src/dhdp/run_hdp.sh
@@ -5,24 +5,54 @@ set -a; . ../.env; set +a
 
 # relevants paths
 data_path="${CORPUS}"
-files=$(find ${data_path} -name *.mm)
 dir_to_save="${MODEL_PATH}"
 
 # clean previous results
 rm -rf $dir_to_save
 mkdir $dir_to_save
 
-# run hdp over each epoch
+##### parallelizing hdp #####
 hdp="hdp/hdp/./hdp"
-N=$(echo $files | wc -w)
-for file in $files
-do
+
+# function to run hdp
+hdp(){
+    file=$1
     # get epoch
     epoch=$(echo $file| cut -d'_' -f 2| cut -d'.' -f 1)
     # path to save model results
     path_to_save="${dir_to_save}/model_${epoch}"
     mkdir $path_to_save
     # run hdp
-    echo "***Step: ${epoch}/${N}***"
-    $hdp --algorithm train --data $file --directory $path_to_save --save_lag -1 --random_seed 123 --max_iter $MAX_ITER --split_merge yes
-done
+    $hdp --algorithm train --data $file --directory $path_to_save --save_lag -1 --random_seed 123 --max_iter $MAX_ITER 
+}
+
+# initialize a semaphore with a given number of tokens
+open_sem(){
+    mkfifo pipe-$$
+    exec 3<>pipe-$$
+    rm pipe-$$
+    local i=$1
+    for((;i>0;i--)); do
+        printf %s 000 >&3
+    done
+}
+
+# run the given command asynchronously and pop/push tokens
+run_with_lock(){
+    local x
+    # this read waits until there is something to read
+    read -u 3 -n 3 x && ((0==x)) || exit $x
+    (
+     ( "$@"; )
+    # push the return code of the command to the semaphore
+    printf '%.3d' $? >&3
+    )&
+}
+
+# run code
+open_sem $CORES
+files=$(find ${data_path} -name *.mm)
+for file in $files
+do
+    run_with_lock hdp $file
+done 

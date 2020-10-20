1bf1fcdcc7d1a500acbed95b799c08ee46a67760
Diego Garrido <diego.garrido.6568@gmail.com>
2020-10-19 23:47:46 -0300

    Save raw corpus per epoch

--- src/processing/processing.py
+++ src/processing/processing.py
@@ -52,11 +52,10 @@ else:
     lemmatization = None
 
 # make folder to export results
-path_to_save = f'{os.getenv("CORPUS")}'
-if os.path.exists(path_to_save):
+if os.path.exists(os.getenv("CORPUS")):
     # remove pre executions
-    shutil.rmtree(path_to_save)
-os.makedirs(path_to_save)
+    shutil.rmtree(os.getenv("CORPUS"))
+os.makedirs(os.getenv("CORPUS"))
 
 logger.info("Data Processing")
 ti = time()
@@ -66,13 +65,13 @@ for epoch in epochs:
     logger.info(f"Processing docs in epoch: {epoch}")
     
     # docs from actual epoch
-    docs = df[df["epoch"] == epoch]["text"]
-    logger.info(f"Corpus size: {len(docs)}")
+    raw_corpus = list(df[df["epoch"] == epoch]["text"])
+    logger.info(f"Corpus size: {len(raw_corpus)}")
 
     # processing docs from actual epoch
     tokenizer_args = {"stopwords": stopwords, "vocabulary": vocabulary, 
     "homol_dict": homol_dict, "stemming": stemming, "lemmatization":lemmatization}
-    corpus = [tokenizer(doc, **tokenizer_args) for doc in docs]
+    corpus = [tokenizer(doc, **tokenizer_args) for doc in raw_corpus]
     
     logger.info("Extracting Vocabulary")
     # map each word to an id {id->word}
@@ -89,7 +88,9 @@ for epoch in epochs:
     vocab = list(dictionary.token2id.keys())
     corpus = [[word for word in doc if word in vocab] for doc in corpus]
     # remove documents with less than DOC_LEN words
+    raw_corpus = [raw_corpus[i] for i in range(len(raw_corpus)) if len(corpus[i])>=float(os.getenv("DOC_LEN"))]
     corpus = [doc for doc in corpus if len(doc)>=float(os.getenv("DOC_LEN"))]
+    df_corpus = pd.DataFrame({"raw_text":raw_corpus, "proc_text": [" ".join(doc) for doc in corpus]})
     logger.info(f"Corpus size after elimination of docs with less than {os.getenv('DOC_LEN')} words: {len(corpus)}")
 
     logger.info("Getting corpus in Blei’s LDA-C format")
@@ -101,8 +102,9 @@ for epoch in epochs:
     logger.info("Saving Corpus")
     zeros = "0"*(len(str(epochs[-1]))-len(str(epoch)))
     epoch_string = f"{zeros}{epoch}"
-    dictionary.save(f"{path_to_save}dictionary_{epoch_string}.dict")
-    BleiCorpus.serialize(f"{path_to_save}corpus_{epoch_string}.mm", corpus)
+    dictionary.save(f"{os.getenv('CORPUS')}dictionary_{epoch_string}.dict")
+    BleiCorpus.serialize(f"{os.getenv('CORPUS')}corpus_{epoch_string}.mm", corpus)
+    df_corpus.to_pickle(f"{os.getenv('CORPUS')}corpus_{epoch_string}.pkl")
 tf = time()
 logger.info("Data Processing Completed")
 total_time = round(tf-ti)
